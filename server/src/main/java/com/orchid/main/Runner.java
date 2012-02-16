5754ab573dcdc7239613f927878db0a0cdd42fa8
Igor Petruk <igor.petrouk@gmail.com>
2012-02-16 22:11:52 +0200

    Fixed Runner for the app

--- server/src/main/java/com/orchid/main/Runner.java
+++ server/src/main/java/com/orchid/main/Runner.java
@@ -2,8 +2,13 @@ package com.orchid.main;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.util.Modules;
+import com.orchid.logging.LoggingModule;
+import com.orchid.logic.LogicModule;
 import com.orchid.net.server.NetworkServerModule;
+import com.orchid.net.server.main.MainServerModule;
 import com.orchid.net.server.main.NetworkServer;
+import com.orchid.ring.DisruptorModule;
 
 import java.io.File;
 import java.io.IOException;
@@ -33,7 +38,12 @@ class Grapher {
 
 public class Runner {
     public static void main(String[] argv) {
-        Injector injector = Guice.createInjector(new NetworkServerModule());
+        Injector injector = Guice.createInjector(
+                new LoggingModule(),
+                new NetworkServerModule(),
+                new DisruptorModule(),
+                new LogicModule()
+        );
         NetworkServer server = injector.getInstance(NetworkServer.class);
         server.start();
     }
