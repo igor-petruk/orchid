f0456b4b0fb4be53a269d71c82bc1562d1ca2585
Igor Petruk <igor.petrouk@gmail.com>
2012-03-19 13:31:56 +0200

    Added journaling service stub

---
+++ server/src/main/scala/com/orchid/journal.scala
@@ -0,0 +1,27 @@
+package com.orchid.journal
+
+import com.lmax.disruptor.EventHandler
+import com.orchid.ring.RingElement
+import com.orchid.messages.generated.Messages.MessageType._
+
+/**
+ * User: Igor Petruk
+ * Date: 12.03.12
+ * Time: 23:33
+ */
+
+trait JournalComponentApi {
+  def journalHandler:EventHandler[RingElement]
+}
+
+trait JournalComponent extends JournalComponentApi{
+  lazy val journalHandler = new JournalHandler
+  
+  class JournalHandler extends EventHandler[RingElement]{
+    val mutatingMessages = List(MAKE_DIRECTORY, CREATE_FILE, DISCOVER_FILE)
+    
+    def onEvent(event: RingElement, sequence: Long, endOfBatch: Boolean) {
+      println(event.getUserID+": "+event.getMessage)
+    }
+  }
+}
