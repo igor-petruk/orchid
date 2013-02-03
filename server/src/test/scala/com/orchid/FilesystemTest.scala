8fa8c0c57e8fe0b91b53d5bd6c26cc8e203dd51f
Igor Petruk <igor.petrouk@gmail.com>
2013-02-03 18:57:46 +0200

    Removed deprecated methods from tests

--- server/src/test/scala/com/orchid/FilesystemTest.scala
+++ server/src/test/scala/com/orchid/FilesystemTest.scala
@@ -2,7 +2,7 @@ package com.orchid
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.{GivenWhenThen, Spec}
+import org.scalatest.{FunSpec, GivenWhenThen, Spec}
 import java.util.UUID
 import collection.immutable.HashMap
 import util.Random
@@ -15,7 +15,7 @@ import com.orchid.tree.{FilesystemError, FilesystemTreeComponent, Node, Filesyst
  */
 
 @RunWith(classOf[JUnitRunner])
-class FilesystemTest extends Spec with GivenWhenThen{
+class FilesystemTest extends FunSpec with GivenWhenThen{
 
   class FilesystemTestComponent extends FilesystemTreeComponent
   
@@ -23,34 +23,34 @@ class FilesystemTest extends Spec with GivenWhenThen{
     val component = new FilesystemTestComponent
     val filesystem = component.filesystem
   }
- /*
+
   describe("A filesystem "){
     it ("should be empty at the beginning"){
-      given("empty filesystem")
+      Given("empty filesystem")
       val f = fixture
-      then("it is empty")
+      Then("it is empty")
       assert(f.filesystem.root.children.isEmpty === true)
     }
     it ("should not have a non existing file"){
-      given("empty filesystem")
+      Given("empty filesystem")
       val f = fixture
-      when("file is queried")
+      When("file is queried")
       val file = f.filesystem.file("ololo/trololo")
-      then("it should not be found")
+      Then("it should not be found")
       assert(file === None)
     }
     it ("should return root with empty path"){
-      given("empty filesystem")
+      Given("empty filesystem")
       val f = fixture
-      when("empty path is queried")
+      When("empty path is queried")
       val file = f.filesystem.file("")
-      then("it should not be found")
+      Then("it should not be found")
       assert(file === Some(f.filesystem.root))
     }
     it ("should support file creation"){
-      given("empty filesystem")
+      Given("empty filesystem")
       val f = fixture
-      when("file tree is are created")
+      When("file tree is are created")
       val file1 = Node(new UUID(1,1),"file1", true, 0,HashMap());
       val file2 = Node(new UUID(2,2),"file2", true, 0,HashMap());
       val file3 = Node(new UUID(3,3),"file3", true, 0,HashMap());
@@ -63,7 +63,7 @@ class FilesystemTest extends Spec with GivenWhenThen{
       f.filesystem.setFile(file1.name+"/"+file2.name+"/"+file4.name, file5)
 
       def assertFile(path:String){
-        then(path + " should be found")
+        Then(path + " should be found")
         f.filesystem.file(path) match {
           case None=>fail(path+ " not found")
           case _=>
@@ -74,12 +74,6 @@ class FilesystemTest extends Spec with GivenWhenThen{
       assertFile("file1/file2/file3")
       assertFile("file1/file2/file4")
       assertFile("file1/file2/file4/file5/")
-    }
-  }
-   */
-  describe("A benchmark "){
-    it ("should be awesome"){
-      benchmark
     }
   }
 
--- server/src/test/scala/com/orchid/RemoteApiTest.scala
+++ server/src/test/scala/com/orchid/RemoteApiTest.scala
@@ -5,7 +5,7 @@ import messages.generated.Messages.ErrorType
 import org.junit.{Test, After, Before}
 import org.junit.Assert._
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.{GivenWhenThen, Spec}
+import org.scalatest.{FunSpec, fixture, GivenWhenThen, Spec}
 import org.junit.runner.RunWith
 import com.orchid.test.{ServerFixtureSupport, TestFilesystemException, TestApi}
 
@@ -16,7 +16,7 @@ import com.orchid.test.{ServerFixtureSupport, TestFilesystemException, TestApi}
  */
 
 @RunWith(classOf[JUnitRunner])
-class RemoteApiTest extends Spec with GivenWhenThen with ServerFixtureSupport{
+class RemoteApiTest extends FunSpec with GivenWhenThen with ServerFixtureSupport{
 
   def fixture = new {
     startMemoryOnlyServer
@@ -26,44 +26,44 @@ class RemoteApiTest extends Spec with GivenWhenThen with ServerFixtureSupport{
   describe("Remote API interaction"){
     val api = fixture.api
     it ("should allow dir creation"){
-      given("empty filesystem")
-      when("dir1 dir is created")
+      Given("empty filesystem")
+      When("dir1 dir is created")
       val dir1 = api.makeDir("dir1")
-      then("it should be created")
+      Then("it should be created")
       dir1 match {
         case Some(Node(_,"dir1",true,0,_))=>
         case _=> fail()
       }
     }
     it ("should allow subdir creation"){
-      given("dir1 dir")
-      when("dir1/dir2 dir is created")
+      Given("dir1 dir")
+      When("dir1/dir2 dir is created")
       val dir2 = api.makeDir("dir1/dir2")
-      then("it should be created")
+      Then("it should be created")
       dir2 match {
         case Some(Node(_,"dir1/dir2",true,0,_))=>
         case _=> fail()
       }
     }
     it ("should not allow double dir creation"){
-      given("dir1/dir2 dir")
-      when("dir1/dir2 dir is created")
+      Given("dir1/dir2 dir")
+      When("dir1/dir2 dir is created")
       val exception = intercept[TestFilesystemException]{
         api.makeDir("dir1/dir2")
       }
-      then("FILE_EXISTS exception should be thrown")
+      Then("FILE_EXISTS exception should be thrown")
       exception match {
         case TestFilesystemException(ErrorType.FILE_EXISTS,_)=>
         case _ => fail()
       }
     }
     it ("should not allow dir creation in non-existing dirs"){
-      given("dir1 dir")
-      when("dir2/dir1 dir is created")
+      Given("dir1 dir")
+      When("dir2/dir1 dir is created")
       val exception = intercept[TestFilesystemException]{
         api.makeDir("dir2/dir1")
       }
-      then("FILE_NOT_FOUND exception should be thrown")
+      Then("FILE_NOT_FOUND exception should be thrown")
       exception match {
         case TestFilesystemException(ErrorType.FILE_NOT_FOUND,_)=>
         case _ => fail()
